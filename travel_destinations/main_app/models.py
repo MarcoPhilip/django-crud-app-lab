0434f6324c5863f4a6b12624e346ca1fb3852cc0
marcophilip <marcophilipagapito@gmail.com>
2025-08-09 23:17:07 -0700

    display activity for each destinations now functioning. creating the activities form now

--- travel_destinations/main_app/models.py
+++ travel_destinations/main_app/models.py
@@ -18,6 +18,6 @@ class Activity(models.Model):
     destination = models.ForeignKey(Destination, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.TextField()
-
+    
     def __str__(self):
         return self.name
