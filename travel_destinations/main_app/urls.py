1aa91b7c0e8025848a783bbe1f9647a1ae1c47da
marcophilip <marcophilipagapito@gmail.com>
2025-08-09 20:49:22 -0700

    base and home pages in progress

--- travel_destinations/main_app/urls.py
+++ travel_destinations/main_app/urls.py
@@ -1,5 +1,10 @@
-from django.urls import path, include
+
+from django.urls import path
+from . import views
 
 urlpatterns = [
    # Routes go here
+   path('about/', views.about, name='about'),
+   path('', views.home, name='home'),
+   path('destinations', views.destination_index, name='destination_index')
 ]
--- travel_destinations/main_app/views.py
+++ travel_destinations/main_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Destination, Activity
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def destination_index(request):
+    destinations = Destination.objects.filter
+
+    return render(request, 'destinations/index.html', {
+        'destinations': destinations
+    })
