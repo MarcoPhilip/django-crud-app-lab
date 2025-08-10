93e7608cc8c0f2cf21eeac70717a31c693087013
marcophilip <marcophilipagapito@gmail.com>
2025-08-09 20:07:32 -0700

    models created and migrated

---
+++ travel_destinations/main_app/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+class Destination(models.Model):
+    name = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_urls(self):
+        return reverse('destination-detail', kwargs={'destination_id': self.id})
+    
+
+class Activity(models.Model):
+    destination = models.ForeignKey(Destination, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
